7dd4b2b391815247b3dac1cb4e34890334ecc0f9
Fellipe Araujo <fellipe.eng.soft@gmail.com>
2026-07-15 15:47:17 -0300

    test: setup vitest with swc

---
+++ vitest.config.e2e.ts
@@ -0,0 +1,16 @@
+import swc from 'unplugin-swc'
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+  test: {
+    include: ['**/*.e2e-spec.ts'],
+    globals: true,
+    root: './',
+  },
+  resolve: { tsconfigPaths: true },
+  plugins: [
+    swc.vite({
+      module: { type: 'es6' },
+    }),
+  ],
+})
