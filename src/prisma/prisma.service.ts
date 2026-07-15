23ddd5f5adceb440b9b3ba4b34446fb0afbc6a50
Fellipe Araujo <fellipe.eng.soft@gmail.com>
2026-07-15 09:27:35 -0300

    feat: create prisma service and apply ajusts in tsconfig and env vars

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,34 @@
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common'
+import { PrismaPg } from '@prisma/adapter-pg'
+import { PrismaClient } from '../../prisma/generated/prisma/client'
+import { ConfigService } from '@nestjs/config'
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  constructor(configService: ConfigService) {
+    const databaseURL = configService.getOrThrow<string>('DATABASE_URL')
+    const parsedDatabaseUrl = new URL(databaseURL)
+
+    const schema = parsedDatabaseUrl.searchParams.get('schema')
+
+    const adapter = new PrismaPg(
+      { connectionString: databaseURL.toString() },
+      { schema: schema || 'public' },
+    )
+    super({
+      adapter,
+      log: ['warn', 'error'],
+    })
+  }
+
+  onModuleInit() {
+    return this.$connect()
+  }
+
+  onModuleDestroy() {
+    return this.$disconnect()
+  }
+}
