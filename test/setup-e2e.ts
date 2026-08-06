78773d3b0a43c617d81a74cf58b0bd6a3e2ec445
Fellipe Araujo <fellipe.eng.soft@gmail.com>
2026-08-06 17:40:30 -0300

    test: cache persistence

--- test/setup-e2e.ts
+++ test/setup-e2e.ts
@@ -1,46 +1,60 @@
 import { config } from 'dotenv'
 
+import { DomainEvents } from '@/core/events/domain-events'
+import { envSchema } from '@/infra/env/env'
 import { PrismaPg } from '@prisma/adapter-pg'
+import { Redis } from 'ioredis'
 import { execSync } from 'node:child_process'
 import { randomUUID } from 'node:crypto'
 import { afterAll } from 'vitest'
 import { PrismaClient } from '../prisma/generated/prisma/client'
-import { DomainEvents } from '@/core/events/domain-events'
 
 config({ path: '.env', override: true })
 config({ path: '.env.test', override: true })
 
 function generateUniqueDatabaseURL(schemaId: string) {
-  if (!process.env.DATABASE_URL) {
+  if (!env.DATABASE_URL) {
     throw new Error('Please provide a DATABASE_URL environment variable')
   }
 
-  const url = new URL(process.env.DATABASE_URL)
+  const url = new URL(env.DATABASE_URL)
 
   url.searchParams.set('schema', schemaId)
 
   return url.toString()
 }
 
+const env = envSchema.parse(process.env)
 const schemaId = randomUUID()
 const databaseURL = generateUniqueDatabaseURL(schemaId)
+const redis = new Redis({
+  host: env.REDIS_HOST,
+  port: env.REDIS_PORT,
+  db: env.REDIS_DB,
+})
 
 process.env.DATABASE_URL = databaseURL
-
-DomainEvents.shouldRun = false
-
-execSync('npx prisma migrate deploy', {
-  env: {
-    ...process.env,
-    DATABASE_URL: databaseURL,
-  },
-  stdio: 'inherit',
-})
 
 const prisma = new PrismaClient({
   adapter: new PrismaPg({
     connectionString: databaseURL,
   }),
+})
+
+beforeAll(async () => {
+  process.env.DATABASE_URL = databaseURL
+
+  DomainEvents.shouldRun = false
+
+  await redis.flushdb()
+
+  execSync('npx prisma migrate deploy', {
+    env: {
+      ...process.env,
+      DATABASE_URL: databaseURL,
+    },
+    stdio: 'inherit',
+  })
 })
 
 afterAll(async () => {
