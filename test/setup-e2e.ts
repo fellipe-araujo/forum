d0d5d407c60c3053c334360a9b0c982a4c154855
Fellipe Araujo <fellipe.eng.soft@gmail.com>
2026-08-06 16:25:37 -0300

    feat: dispatch domain events

--- test/setup-e2e.ts
+++ test/setup-e2e.ts
@@ -5,6 +5,7 @@ import { execSync } from 'node:child_process'
 import { randomUUID } from 'node:crypto'
 import { afterAll } from 'vitest'
 import { PrismaClient } from '../prisma/generated/prisma/client'
+import { DomainEvents } from '@/core/events/domain-events'
 
 config({ path: '.env', override: true })
 config({ path: '.env.test', override: true })
@@ -25,6 +26,8 @@ const schemaId = randomUUID()
 const databaseURL = generateUniqueDatabaseURL(schemaId)
 
 process.env.DATABASE_URL = databaseURL
+
+DomainEvents.shouldRun = false
 
 execSync('npx prisma migrate deploy', {
   env: {
